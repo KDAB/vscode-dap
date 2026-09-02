f4257fc873ab0289cc95886729f919ce91c85d78
Sergio Martins <sergio.martins@kdab.com>
2026-09-02 23:51:39 +0100

    chore: Package name now starts with kdab- prefix

--- upload_package.sh
+++ upload_package.sh
@@ -3,7 +3,7 @@
 # SPDX-FileCopyrightText: 2026 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
 # SPDX-License-Identifier: MIT
 
-# Uploads the dap-<version>.vsix package to GitHub Release
+# Uploads the kdab-dap-<version>.vsix package to GitHub Release
 # Called by .github/workflows/package.yml
 
 set -e
@@ -11,7 +11,7 @@ set -e
 VERSION=$(jq -r '.version' package.json)
 TAG_NAME=v$VERSION
 
-PACKAGE_FILENAME=dap-$VERSION.vsix
+PACKAGE_FILENAME=kdab-dap-$VERSION.vsix
 
 if [ ! -f "$PACKAGE_FILENAME" ]; then
     # Doesn't happen. Package is created by package.yml
